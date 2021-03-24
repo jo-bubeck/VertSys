3c91cc356ea300574eccd507856687e41ae3a4d1
jo-bubeck <johannes.bubeck@gmx.net>
2021-03-24 11:28:22 +0100

    Entities

---
+++ backend/src/main/java/de/dhbw/wwi19b4/backend/model/Author.java
@@ -0,0 +1,29 @@
+package de.dhbw.wwi19b4.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(length = 100)
+    @NotNull(message = "Der Name muss gesetzt sein!")
+    @Size(min = 1, max = 100)
+    private String name = "";
+
+    private Date dateOfBirth;
+}
